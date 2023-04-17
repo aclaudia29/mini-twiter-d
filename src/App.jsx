55e59b99c4438b244af5e57c37c30403d0a8aeb2
claudia <claudiaalbarracin@gmail.com>
2023-04-18 00:17:12 +0200

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+///import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <div className='principal'>
+        <h1>AQUI COMIENZA NUESTRO PROYECTO</h1>
+        
+         
+ </div>
+        
+        
+ <h2>AQUI IMAGEN </h2>
+          
+
+    </div>
+  );
+}
+
+export default App
